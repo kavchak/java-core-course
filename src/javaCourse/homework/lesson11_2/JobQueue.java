1e80ac1de8b30d468c73b3c9e1ec0a9dddbdc05a
kavchak <kavchak@users.noreply.github.com>
2025-02-25 22:49:32 +0200

    homework: lesson 11 task 2
    Please review code changes and approve

---
+++ src/javaCourse/homework/lesson11_2/JobQueue.java
@@ -0,0 +1,19 @@
+package javaCourse.homework.lesson11_2;
+
+import java.util.ArrayList;
+
+public class JobQueue {
+    ArrayList<Runnable> jobs = new ArrayList<>();
+
+    public synchronized void put(Runnable job){
+        jobs.add(job);
+        this.notifyAll();
+    }
+
+    public synchronized Runnable getJob() throws InterruptedException {
+        while (jobs.size() == 0) {
+            this.wait();
+        }
+        return jobs.remove(0);
+    }
+}

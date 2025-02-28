58bb2da4681414a20b6f571cadcce97c9959d8db
kavchak <kavchak@users.noreply.github.com>
2025-02-28 16:34:39 +0200

    classwork: lesson 12
    Please review code changes and approve

---
+++ src/javaCourse/classwork/lesson12/AnnotationTest.java
@@ -0,0 +1,10 @@
+package javaCourse.classwork.lesson12;
+
+@MyAnnotation("Nice")
+public class AnnotationTest {
+
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/javaCourse/classwork/lesson12/cookies/Bakery.java
@@ -0,0 +1,48 @@
+package javaCourse.classwork.lesson12.cookies;
+
+import java.lang.reflect.Method;
+
+public class Bakery {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+
+        Class cl = Class.forName("javaCourse.classwork.lesson12.cookies.Cookie");
+
+        if (cl.isAnnotationPresent(ControlledObject.class)) {
+            System.out.println("annotation is present");
+
+            ControlledObject annotation = (ControlledObject) cl.getAnnotation(ControlledObject.class);
+            System.out.println(annotation);
+
+            System.out.println(annotation.name());
+
+            boolean isStart = false;
+            boolean isStop = false;
+
+            Method[] methods = cl.getMethods();
+            for (Method method: methods) {
+                if (method.isAnnotationPresent(StartObject.class)) {
+                    isStart = true;
+                }
+                if (method.isAnnotationPresent(StopObject.class)) {
+                    isStop = true;
+                }
+
+                if (isStart && isStop) {
+                    break;
+                }
+            }
+
+            System.out.println("This class can start baking: " + isStart);
+            System.out.println("This class can stop baking: " + isStop);
+
+            if (isStart && isStop) {
+                Cookie cookie = new Cookie();
+                cookie.cook(annotation.name());
+                cookie.finish(annotation.name());
+            }
+        } else {
+            System.out.println("not annotated");
+        }
+    }
+}
